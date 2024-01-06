9a9057fc6cb662af8aecf553ecd6cbbef6e366d7
Roy van de Water <roy.v.water@gmail.com>
2024-01-06 10:59:09 -0700

    Fetch issues by team
    Expect config at ~/.config/linear/config.toml
    Update README with setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # linear
 CLI for interacting with Linear tickets
+
+## Running
+
+You'll need to create a config file at `~/.config/linear/config.toml` that provides your API key & team name. API keys can be generated at https://linear.app/sibi/settings/api.
+
+```toml
+# ~/.config/linear/config.toml
+api_key = "<api-key>"
+team_name = "Headless"
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use std::fs;
+extern crate derive_error;
 
-use graphql_client::{GraphQLQuery, Response};
+use derive_error::Error;
+use std::{
+    env::{self},
+    fs,
+};
+
+use graphql_client::{GraphQLQuery, QueryBody, Response};
 use reqwest::{self};
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Config {
-    api_key: String,
-}
+use serde::{Deserialize, Serialize};
 
 fn main() {
-    let config_str = fs::read_to_string("config.toml").expect("Unable to read file config.toml");
-    let config: Config = toml::from_str(&config_str).expect("Unable to parse config.toml");
+    let config = get_config().expect("Unable to get config");
 
-    fetch_issues(&config.api_key)
+    let response =
+        fetch_issues(&config.api_key, &config.team_name).expect("Failed to fetch issues");
+    let data = response.data.expect("Did not receive data");
+
+    for edge in data.issues.edges {
+        let issue = edge.node;
+        println!("{}: {}", issue.identifier, issue.title);
+    }
 }
 
 #[derive(GraphQLQuery)]
@@ -24,17 +31,58 @@ fn main() {
 )]
 pub struct IssuesQuery;
 
-fn fetch_issues(api_key: &str) {
-    let request_body = IssuesQuery::build_query(issues_query::Variables {});
+#[derive(Debug, Error)]
+enum FetchError {
+    ReqwestError(reqwest::Error),
+}
+
+fn fetch<T: for<'a> Deserialize<'a>, V: Serialize>(
+    api_key: &str,
+    query_body: &QueryBody<V>,
+) -> Result<Response<T>, FetchError> {
     let client = reqwest::blocking::Client::new();
-    let res = client
+
+    let response = client
         .post("https://api.linear.app/graphql")
         .header("Authorization", api_key)
-        .json(&request_body)
-        .send()
-        .expect("Request failed");
+        .json(query_body)
+        .send()?
+        .json()?;
 
-    let response_body: Response<issues_query::ResponseData> = res.json().expect("Unable to parse");
+    return Ok(response);
+}
 
-    println!("data: {:?}", response_body.data);
+fn fetch_issues(
+    api_key: &str,
+    team_name: &str,
+) -> Result<Response<issues_query::ResponseData>, FetchError> {
+    let request_body = IssuesQuery::build_query(issues_query::Variables {
+        team_name: team_name.to_string(),
+    });
+    let response: Response<issues_query::ResponseData> = fetch(api_key, &request_body)?;
+
+    return Ok(response);
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    api_key: String,
+    team_name: String,
+}
+
+#[derive(Debug, Error)]
+enum ConfigError {
+    EnvVarError(env::VarError),
+    FileReadError(std::io::Error),
+    ParseError(toml::de::Error),
+}
+
+fn get_config() -> Result<Config, ConfigError> {
+    let home = env::var("HOME")?;
+    let file_path = format!("{}/.config/linear/config.toml", home);
+
+    let config_str = fs::read_to_string(file_path)?;
+    let config: Config = toml::from_str(&config_str)?;
+
+    return Ok(config);
 }
