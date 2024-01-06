91e0094550da53a37585ed3ef6c63ccadf86d634
Roy van de Water <roy.v.water@gmail.com>
2024-01-06 14:41:08 -0700

    Move issues listing into subcommand

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,44 @@ mod config;
 mod graphql_fetch;
 mod issues;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "linear", version)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Issues,
+}
+
 fn main() {
     let config = config::get_config().expect("Unable to get config");
     let api_key = config.api_key;
     let team_name = config.team_name;
 
-    let issues = issues::fetch(&api_key, &team_name).expect("Failed to fetch issues");
+    let args = Args::parse();
+    match args.command {
+        Command::Issues => list_issues(&api_key, &team_name),
+    }
+}
 
-    println!("\n");
+fn list_issues(api_key: &str, team_name: &str) {
+    let issues = issues::fetch(api_key, team_name).expect("Failed to fetch issues");
+
     println!("In Progress");
     println!("===========");
     for issue in issues.in_progress {
-        println!("{}: {}", issue.identifier, issue.title);
+        println!("[{}] {}", issue.identifier, issue.title);
     }
 
     println!("\n");
     println!("Todo");
     println!("====");
     for issue in issues.todo {
-        println!("{}: {}", issue.identifier, issue.title);
+        println!("[{}] {}", issue.identifier, issue.title);
     }
 }
